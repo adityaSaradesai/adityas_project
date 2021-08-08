d10bc4fde95ccefbe943988113d72e5a6b84ec78
adityaSaradesai <addi1430@gmail.com>
2021-08-09 00:06:41 +0530

    This is my first commit

---
+++ Projects/src/test/java/com/TY/Rmg/Projects/CreateProjectTest.java
@@ -0,0 +1,12 @@
+package com.TY.Rmg.Projects;
+
+import org.testng.annotations.Test;
+
+public class CreateProjectTest {
+	@Test
+	public void createProject()  {
+		
+		System.out.println("create Project");
+	}
+
+}
---
+++ Projects/src/test/java/com/TY/Rmg/Projects/UpdateProject.java
@@ -0,0 +1,11 @@
+package com.TY.Rmg.Projects;
+
+import org.testng.annotations.Test;
+
+public class UpdateProject {
+	@Test
+	public void upDateProject() {
+		System.out.println("update project");
+	}
+
+}
